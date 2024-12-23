b4759f7b2746bef01bfbc9351729621180a74406
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2024-12-23 19:42:40 +0500

    add auto play in landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,12 +1,19 @@
-import { Button } from "@/components/ui/button"
-import { Link } from "react-router-dom"
-
+/* eslint-disable no-unused-vars */
+import { Button } from "@/components/ui/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import { Link } from "react-router-dom";
+import companies from "../data/companies";
+import Autoplay from "embla-carousel-autoplay";
 
 const LandingPage = () => {
   return (
     <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-20">
       <section className="text-center ">
-      <h1 className="flex flex-col items-center justify-center gradient-title font-extrabold text-4xl sm:text-6xl lg:text-8xl tracking-tighter py-4">
+        <h1 className="flex flex-col items-center justify-center gradient-title font-extrabold text-4xl sm:text-6xl lg:text-8xl tracking-tighter py-4">
           Find Your Dream Job
           <span className="flex items-center gap-2 sm:gap-6">
             and get
@@ -33,7 +40,27 @@ const LandingPage = () => {
           </Button>
         </Link>
       </div>
+      <Carousel
+        plugins={[
+          Autoplay({
+            delay: 2000,
+          }),
+        ]}
+        className="w-full py-10"
+      >
+        <CarouselContent className="flex gap-5 sm:gap-20 items-center">
+          {companies.map(({ name, id, path }) => (
+            <CarouselItem key={id} className="basis-1/3 lg:basis-1/6 ">
+              <img
+                src={path}
+                alt={name}
+                className="h-9 sm:h-14 object-contain"
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
     </main>
-  )
-}
-export default LandingPage
+  );
+};
+export default LandingPage;
